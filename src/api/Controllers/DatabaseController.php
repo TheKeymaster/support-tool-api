44bdddc1cbbfa0022867f6f3eacf39e5e58e0794
Dominik Brader <d.brader@findologic.com>
2018-09-16 21:20:39 +0200

    Add some stuff

--- src/api/Controllers/DatabaseController.php
+++ src/api/Controllers/DatabaseController.php
@@ -10,6 +10,15 @@ class DatabaseController
     /** @var string */
     const DB_NAME = 'support_system';
 
+    /** @var string */
+    const DB_ERROR_MESSAGE = 'An unknown error occurred!';
+
+    /** @var string */
+    const STATUS_SUCCESS = 'success';
+
+    /** @var string */
+    const STATUS_ERROR = 'error';
+
     /** @var string $host */
     private $host;
 
@@ -19,8 +28,8 @@ class DatabaseController
     /** @var string $password */
     private $password;
 
-    /** @var mysqli $connection */
-    private $connection;
+    /** @var mysqli $mysqli */
+    private $mysqli;
 
     /**
      * DatabaseController constructor.
@@ -41,9 +50,145 @@ class DatabaseController
                 $conn->connect_error));
         }
 
-        $this->connection = $conn;
+        $this->mysqli = $conn;
         $this->host = $host;
         $this->username = $username;
         $this->password = $password;
+    }
+
+    /**
+     * Creates a new entry in a specific table.
+     *
+     * @param string $tableName
+     * @param array $fields
+     * @param array $values
+     * @return array
+     */
+    public function create($tableName, array $fields, array $values)
+    {
+        if (!is_string($tableName)) {
+            throw new InvalidArgumentException('Invalid type for table name submitted!');
+        }
+
+        $query = "INSERT INTO $tableName ($fields) VALUES ($values)";
+        if ($this->mysqli->query($query)) {
+            $successMessage = 'Row(s) were successfully created!';
+            $affectedRows = $this->mysqli->affected_rows;
+            return $this->createStatusMessage(self::STATUS_SUCCESS, $successMessage, $affectedRows);
+        } else {
+            return $this->unknownDatabaseError();
+        }
+    }
+
+    /**
+     * Reads data from a specific table.
+     *
+     * @param string|array $cols
+     * @param string $tableName
+     * @param int $limit
+     * @param string|null $condition Condition without "where" e.g. 'id = 123'.
+     * @return array
+     */
+    public function read($cols, $tableName, $limit = 1000, $condition = null)
+    {
+        if (!is_string($tableName)) {
+            throw new InvalidArgumentException('Invalid type for table name submitted!');
+        }
+
+        if ($condition === null) {
+            $query = "SELECT $cols FROM $tableName LIMIT $limit";
+        } elseif (is_string($condition)) {
+            $query = "SELECT $cols FROM $tableName WHERE $condition LIMIT $limit";
+        } else {
+            throw new InvalidArgumentException('Invalid type for condition submitted!');
+        }
+
+        if ($result = $this->mysqli->query($query)) {
+            if ($result->num_rows > 0) {
+                $message = 'Row(s) found!';
+                $results = $this->createStatusMessage(self::STATUS_SUCCESS, $message, $result->num_rows);
+                while ($row = $result->fetch_assoc()) {
+                    $results[] = $row;
+                }
+                return $results;
+            } else {
+                $message = 'No rows are matching your query!';
+                return $this->createStatusMessage(self::STATUS_SUCCESS, $message, 0);
+            }
+        } else {
+            return $this->unknownDatabaseError();
+        }
+    }
+
+    /**
+     * Updates data from a specific table.
+     *
+     * @param string $tableName
+     * @param string $set Condition without "set" e.g. 'lastname="Joe"'.
+     * @param string $condition Condition without "where" e.g. 'id = 123'.
+     * @return array
+     */
+    public function update($tableName, $set, $condition)
+    {
+        if (!(is_string($tableName) && is_string($set) && is_string($condition))) {
+            throw new InvalidArgumentException('Type of table name, set and/or where is invalid!');
+        }
+
+        $query = "UPDATE $tableName SET $set WHERE $condition";
+
+        if ($this->mysqli->query($query)) {
+            $message = 'Row(s) were updated successfully!';
+            return $this->createStatusMessage(self::STATUS_SUCCESS, $message, $this->mysqli->affected_rows);
+        } else {
+            return $this->unknownDatabaseError();
+        }
+    }
+
+    /**
+     * Deletes data from a specific table.
+     *
+     * @param $tableName
+     * @param $condition
+     * @return array
+     */
+    public function delete($tableName, $condition)
+    {
+        if (!(is_string($tableName) && is_string($condition))) {
+            throw new InvalidArgumentException('Type of table name, set and/or where is invalid!');
+        }
+
+        $query = "DELETE FROM $tableName WHERE $condition";
+
+        if ($this->mysqli->query($query)) {
+            $message = 'Row(s) were deleted successfully!';
+            return $this->createStatusMessage(self::STATUS_SUCCESS, $message, $this->mysqli->affected_rows);
+        } else {
+            return $this->unknownDatabaseError();
+        }
+    }
+
+    /**
+     * This function is being called if the SQL query returns an error.
+     *
+     * @return array
+     */
+    private function unknownDatabaseError()
+    {
+        return $this->createStatusMessage(self::STATUS_ERROR, self::DB_ERROR_MESSAGE, 0);
+    }
+
+    /**
+     * @param string $status
+     * @param string $message
+     * @param int $affectedRows
+     * @return array
+     */
+    private function createStatusMessage($status, $message, $affectedRows)
+    {
+        return [
+            'status' => $status,
+            'message' => $message,
+            'rows_affected' => $affectedRows,
+        ];
     }
 }
