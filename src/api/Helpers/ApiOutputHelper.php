e1a108d3b8166e218223d85635b46a2bd10acfc8
Dominik Brader <d.brader@findologic.com>
2018-10-03 11:57:57 +0200

    Add necassary headers

--- src/api/Helpers/ApiOutputHelper.php
+++ src/api/Helpers/ApiOutputHelper.php
@@ -13,7 +13,10 @@ class ApiOutputHelper
     public static function setJsonHeader()
     {
         header('Content-Type:application/json;charset=utf8');
-        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin:http://support-frontend.dom.olo');
+        header('Access-Control-Allow-Methods:GET,PUT,POST,DELETE,PATCH,OPTIONS,Access-Control-Allow-Origin');
+        header('Access-Control-Allow-Headers:Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Credentials, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+        header('Access-Control-Allow-Credentials:true');
     }
 
     /**
