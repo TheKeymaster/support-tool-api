1555ee71c8290fd42d568932cfb6b86289929523
Dominik Brader <d.brader@findologic.com>
2018-10-03 12:03:21 +0200

    Get image url from gravatar

--- src/api/Endpoints/get/user.php
+++ src/api/Endpoints/get/user.php
@@ -14,5 +14,6 @@ $_GET['authkey'] = isset($_GET['authkey']) ? $_GET['authkey'] : null;
 
 $requester = $outputController->sendAuthkeyRequest($_GET['authkey']);
 $result = $outputController->sendUserIdRequest($requester);
+$result[0]['imageurl'] = sprintf('https://www.gravatar.com/avatar/%s', md5($result[0]['email']));
 
 echo json_encode($result);
