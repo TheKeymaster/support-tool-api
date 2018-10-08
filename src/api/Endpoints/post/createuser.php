6699e0681abbd350f1b2c689c5a439cc8d3b2fb4
Dominik Brader <d.brader@findologic.com>
2018-10-08 16:52:20 +0200

    Changes WIP

--- src/api/Endpoints/post/createuser.php
+++ src/api/Endpoints/post/createuser.php
@@ -17,10 +17,15 @@ $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : false;
 $password = isset($_POST['password']) ? $_POST['password'] : false;
 
 if ($email && $firstname && $lastname && $password) {
-    $authkey = $outputController->generateAuthKey();
-    $password = password_hash($password, PASSWORD_BCRYPT);
-    $result = $databaseController->create('user', ['email', 'firstname', 'lastname', 'password', 'role', 'authkey'], [$email, $firstname, $lastname, $password, 1, $authkey]);
-    $result['result'] = true;
+    $userExists = $databaseController->execCustomSqlQuery("SELECT * FROM user WHERE email = $email");
+    if ($userExists) {
+        $result['result'] = false;
+    } else {
+        $authkey = $outputController->generateAuthKey();
+        $password = password_hash($password, PASSWORD_BCRYPT);
+        $result = $databaseController->create('user', ['email', 'firstname', 'lastname', 'password', 'role', 'authkey'], [$email, $firstname, $lastname, $password, 1, $authkey]);
+        $result['result'] = true;
+    }
 } else {
     $result['result'] = false;
 }
