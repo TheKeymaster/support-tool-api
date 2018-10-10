5270919e6bbde84d9c693cf134b8595db781d686
Dominik Brader <d.brader@findologic.com>
2018-10-10 09:07:04 +0200

    Set SMTPDebug back to 0

--- src/api/Controllers/MailController.php
+++ src/api/Controllers/MailController.php
@@ -81,7 +81,7 @@ class MailController
 
     private function setDefaultConfiguration() {
         $this->mail->isSMTP();
-        $this->mail->SMTPDebug = 2;
+        $this->mail->SMTPDebug = 0;
         $this->mail->Host = self::DEFAULT_GOOGLE_HOST;
         $this->mail->Port = self::DEFAULT_GOOGLE_PORT;
         $this->mail->SMTPSecure = self::TLS_ENCRYPTION;
